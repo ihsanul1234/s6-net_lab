e67c828f385eec5483004b094197de0e22bbf486
Ihsanul Haq <131159276+ihsanul1234@users.noreply.github.com>
2025-02-18 09:42:26 +0530

    Program written using poco m4 pro (termux)

--- conudps.c
+++ conudps.c
@@ -12,5 +12,31 @@
 //Driver code
 int main()
 {
-  int sockfd,cp,t;
+	int sockfd,cp,t;
+	char buffer[MAXLINE];
+	time_t ct;
+	struct sockaddr_in servaddr,cliaddr;
+	//creating socket file descriptor
+	if(sockfd=socket(AF_INET,SOCK_DGRAM,0))<0)
+	{
+		perror("socket creation failed!");
+		exit(EXIT_FAILURE);
+	}
+	memset(&servaddr,0,sizeof(servaddr));
+	memset(&cliaddr,0,sizeof(cliaddr));
+	//filling server information
+	servaddr.sin_family=AF_INET; //IPV4
+	servaddr.sin_addr.s_addr=INADDR_ANY;
+	servaddr.sin_port=htons(PORT);
+	//Bind the socket with the server address
+	if(bind(sockfd,(const struct sockaddr*)&servaddr,sizeof(servaddr))<0)
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	int len,n;
+	len=sizeof(cliaddr);//len is value/result
+	while(1)
+	{
+		n=recvfrom(sockfd,(char*)buffer,MAXLINE
 
